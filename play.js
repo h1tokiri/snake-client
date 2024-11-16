59e3630a6bbe10b1f8a81d08fc89c685faf09785
Javin Leslie Lau <javinlau21@gmail.com>
2024-11-16 18:26:10 -0500

    v3 play.js

--- play.js
+++ play.js
@@ -1,4 +1,6 @@
 const { connect } = require("./client");
 
+const name = process.argv[2] || "SNK"; // default to SNK if no argumetn is given
+
 console.log("Connecting ...");
-connect();
+connect(name);
