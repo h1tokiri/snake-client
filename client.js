722fb4c9cdef8bb644229fe454f8819eaa247260
Javin Leslie Lau <javinlau21@gmail.com>
2024-11-16 17:46:23 -0500

    v1 client.js

---
+++ client.js
@@ -0,0 +1,25 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: "localhost", // IP address here,
+    port: 50541, // PORT number here,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on("connect", () => {
+    console.log("Successfully connected to the game server!");
+    conn.write("Name: Player One");
+  });
+
+  conn.on("data", (data) => {
+    console.log("Message from server:", data);
+  });
+
+  return conn;
+};
+
+module.exports = { connect };
