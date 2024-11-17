37290863165647a1b3e8146dfa326924f16109f1
Javin Leslie Lau <javinlau21@gmail.com>
2024-11-16 22:38:48 -0500

    16 2238 Nov 24 play.js

--- play.js
+++ play.js
@@ -1,25 +1,10 @@
 const { connect } = require("./client");
 
+const { setupInput } = require("./input");
+
 const name = process.argv[2] || "SNK"; // default to SNK if no argumetn is given
 
 console.log("Connecting ...");
 connect(name);
-
-const setupInput = function () {
-  const stdin = process.stdin; // create variable to hold the stdin object so we don't have to type process.stdin multiple times
-  stdin.setRawMode(true); // raw mode allows us to listen for individual keypresses instead of waiting for user to press enter
-  stdin.setEncoding("utf8"); // utf8 encoding is set so that we can read the text data that is input
-  stdin.resume(); // resume stdin so the program can listen for input
-  stdin.on("data", handleUserInput);
-  return stdin; // return the stdin object so we can use it elsewhere in the program
-};
-
-const handleUserInput = function (key) {
-  // code handling the key press will go here
-  if (key === '\u0003') { // (this is ctrl + c)
-    console.log("You've quit the game!");
-    process.exit();
-  }
-};
 
 setupInput();
