1f465415d70cd1d16a6f9c3cfa1af2604da3877d
Javin Leslie Lau <javinlau21@gmail.com>
2024-11-17 21:45:50 -0500

    17 2145 Nov 24 input.js

--- input.js
+++ input.js
@@ -1,11 +1,28 @@
-const setupInput = function () {
+let connection;
+
+// const conn = connect(name);
+
+const setupInput = (conn) => {
+  connection = conn;
+  console.log("Connection object in setupInput:", connection); //debug log
+  // setupInput(conn);
   const stdin = process.stdin; // create variable to hold the stdin object so we don't have to type process.stdin multiple times
   stdin.setRawMode(true); // raw mode allows us to listen for individual keypresses instead of waiting for user to press enter
   stdin.setEncoding("utf8"); // utf8 encoding is set so that we can read the text data that is input
   stdin.resume(); // resume stdin so the program can listen for input
+  
   stdin.on("data", handleUserInput);
   return stdin; // return the stdin object so we can use it elsewhere in the program
 };
+
+// const setupInput = function () {
+//   const stdin = process.stdin; // create variable to hold the stdin object so we don't have to type process.stdin multiple times
+//   stdin.setRawMode(true); // raw mode allows us to listen for individual keypresses instead of waiting for user to press enter
+//   stdin.setEncoding("utf8"); // utf8 encoding is set so that we can read the text data that is input
+//   stdin.resume(); // resume stdin so the program can listen for input
+//   stdin.on("data", handleUserInput);
+//   return stdin; // return the stdin object so we can use it elsewhere in the program
+// };
 
 const handleUserInput = function (key) {
   // code handling the key press will go here
@@ -13,6 +30,13 @@ const handleUserInput = function (key) {
     console.log("You've quit the game!");
     process.exit();
   }
+  
+  console.log("Current connection object in handleUserInput:", connection);
+
+  if (key === 'w') connection.write("Move: up");
+  if (key === 'a') connection.write("Move: left");
+  if (key === 's') connection.write("Move: down");
+  if (key === 'd') connection.write("Move: right");
 };
 
 module.exports = { setupInput };
